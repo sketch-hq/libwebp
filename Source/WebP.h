4e0dd1cad67946f3e703da4ef7fe329d8f61632e
Gavin MacLean <gavin@openplanetsoftware.com>
2016-09-02 15:21:08 +0100

    Added Xcode project

---
+++ Source/WebP.h
@@ -0,0 +1,10 @@
+//  Created by Gavin MacLean on 30/08/2016.
+//  Copyright (c) 2014 Bohemian Coding. All rights reserved.
+
+#include "encode.h"
+#include "decode.h"
+#include "demux.h"
+#include "format_constants.h"
+#include "mux_types.h"
+#include "mux.h"
+#include "types.h"
---
+++ Tests/WebPTester.m
@@ -0,0 +1,27 @@
+//
+//  WebPTester.m
+//  WebP
+//
+//  Created by Gavin on 30/08/2016.
+//  Copyright © 2016 Bohemian Coding. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WebPTester : ECTestCase
+
+@end
+
+@implementation WebPTester
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+@end
