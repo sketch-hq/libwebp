48dac9292536ebb0693ba81f7f90e8b705f5e1ae
Gavin MacLean <gavin@openplanetsoftware.com>
2016-09-06 13:09:40 +0100

    Fix up imported webP data with an alpha of 0 so that the colour channels are also 0

--- Source/NSImage+WebP.m
+++ Source/NSImage+WebP.m
@@ -15,17 +15,21 @@
   if (data) {
     int width = 0;
     int height = 0;
-    uint8_t *webp = WebPDecodeRGBA( data.bytes, data.length, &width, &height );
+    UInt8 *webp = WebPDecodeRGBA( data.bytes, data.length, &width, &height );
     if (webp) {
-      CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-      CGContextRef bitmap = CGBitmapContextCreate( webp, width, height, 8, 4 * width, colorSpace, kCGImageAlphaPremultipliedLast );
-      CFRelease(colorSpace);
-      
-      CGImageRef cgImage = CGBitmapContextCreateImage( bitmap );
-      result = [[NSImage alloc] initWithCGImage:cgImage size:NSMakeSize(width, height)];
-      CFRelease( cgImage );
-      CFRelease( bitmap );
-      
+      UInt32 *fixup = (UInt32*)webp;
+      // If the alpha channel is 0, webP seems to take the view that it doesn't matter what's in it.
+      // Setting this to 0 for consistency with (e.g) loading from PNG.
+      for (int i = 0; i < width * height; i++ ) {
+        if (!(fixup[i] & 0xFF000000)) {
+          fixup[i] = 0x00000000;
+        }
+      }
+
+      NSBitmapImageRep *rep = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:&webp pixelsWide:width pixelsHigh:height bitsPerSample:8 samplesPerPixel:4 hasAlpha:YES isPlanar:NO colorSpaceName:NSDeviceRGBColorSpace bitmapFormat:NS32BitLittleEndianBitmapFormat bytesPerRow:4 * width bitsPerPixel:32];
+      result = [[NSImage alloc] initWithSize:NSMakeSize(width, height)];
+      [result addRepresentation:rep];
+
       WebPFree( webp );
     }
   }
