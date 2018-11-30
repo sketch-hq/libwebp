e86dd0b1e2b15ca831d224c77f797b1af47d7538
Gavin MacLean <gavin@openplanetsoftware.com>
2018-11-30 12:58:14 +0000

    The alpha in the webP data doesn't have premultiplied alpha #18782

--- Source/NSImage+WebP.m
+++ Source/NSImage+WebP.m
@@ -17,16 +17,7 @@ NSString * const NSImageUTTypeWebP = @"public.webp";
   int height = 0;
   UInt8 *webp = WebPDecodeRGBA( data.bytes, data.length, &width, &height );
   if (webp) {
-    UInt32 *fixup = (UInt32*)webp;
-    // If the alpha channel is 0, webP seems to take the view that it doesn't matter what's in it.
-    // Setting this to 0 for consistency with (e.g) loading from PNG.
-    for (int i = 0; i < width * height; i++ ) {
-      if (!(fixup[i] & 0xFF000000)) {
-        fixup[i] = 0x00000000;
-      }
-    }
-    
-    NSBitmapImageRep *rep = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:NULL pixelsWide:width pixelsHigh:height bitsPerSample:8 samplesPerPixel:4 hasAlpha:YES isPlanar:NO colorSpaceName:NSDeviceRGBColorSpace bitmapFormat:NS32BitLittleEndianBitmapFormat bytesPerRow:4 * width bitsPerPixel:32];
+    NSBitmapImageRep *rep = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:NULL pixelsWide:width pixelsHigh:height bitsPerSample:8 samplesPerPixel:4 hasAlpha:YES isPlanar:NO colorSpaceName:NSDeviceRGBColorSpace bitmapFormat:NS32BitLittleEndianBitmapFormat | NSAlphaNonpremultipliedBitmapFormat bytesPerRow:4 * width bitsPerPixel:32];
     memcpy( rep.bitmapData, webp, width * height * 4 );
     WebPFree( webp );
     result = [[NSImage alloc] initWithSize:NSMakeSize(width, height)];
